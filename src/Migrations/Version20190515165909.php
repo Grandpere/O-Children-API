efcf6fe5f448b797e3b0d61a75165a4f185a7da5
Grandpere <lorenzo.marozzo@gmail.com>
2019-05-15 19:52:14 +0200

    update last migration for comments test on discovery not present on gandi server

--- src/Migrations/Version20190515165909.php
+++ src/Migrations/Version20190515165909.php
@@ -22,7 +22,7 @@ final class Version20190515165909 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE discovery');
+        // $this->addSql('DROP TABLE discovery');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EBA934BCD');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EBA934BCD FOREIGN KEY (quizz_id) REFERENCES quizz (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF');
@@ -34,7 +34,7 @@ final class Version20190515165909 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE discovery (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(150) NOT NULL COLLATE utf8mb4_unicode_ci, cards JSON NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        // $this->addSql('CREATE TABLE discovery (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(150) NOT NULL COLLATE utf8mb4_unicode_ci, cards JSON NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF');
         $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EBA934BCD');
